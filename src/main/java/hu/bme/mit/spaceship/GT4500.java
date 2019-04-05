90acd720a65db5647936e8769533090d691bbf8d
majkutK <kristof.majkut@gmail.com>
2019-04-05 09:54:19 +0200

    Merge conflict feloldas 2

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -78,12 +78,8 @@ public class GT4500 implements SpaceShip {
 
       case ALL:
         // try to fire both of the torpedo stores
-<<<<<<< HEAD
         //TODO implement feature xxx
         firingSuccess = false;
-=======
-        //TODO implement
->>>>>>> branch-B
         firingSuccess = true;
         firingSuccess = true;
 
